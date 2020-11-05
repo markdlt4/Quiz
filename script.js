37b75dda91eea56278edf2957ccd6a8a9adabc37
markt <markdlt4@gmail.com>
2020-11-05 13:58:16 -0600

    Added to JS

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 var startButton = document.getElementById("start");
+var secondsLeft = 10
+var timeEl = document.getElementById("timer")
+
 function promptQuestion(){
-    console.log("click")
+    var timerInterval = setInterval(function() {
+        secondsLeft--; 
+        console.log(secondsLeft)
+        timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+    
+        if(secondsLeft === 0) {
+          clearInterval(timerInterval);
+          console.log("times up")
+        }
+    
+      }, 1000);
 } 
 startButton.addEventListener("click", promptQuestion)
